2743d75ab3b00a9934413a743e06a5e82b27f5ca
Ryan Gaus <rsg1egoman@gmail.com>
2016-11-02 11:09:44 -0400

    code: helping brian with react router

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,9 +6,14 @@ import App from 'dashboard/app'
 import store from 'dashboard/store';
 import "whatwg-fetch"
 
+import {Router, Route, hashHistory} from 'react-router';
+
 ReactDOM.render(
     <Provider store={store}>
-    	<App />
+      <Router history={hashHistory}>
+        <Route path="/" component={App} />
+        <Route path="/forgot-password" component={() => <span>Yo</span>} />
+      </Router>
     </Provider>,
     document.getElementById('react-mount')
 )
