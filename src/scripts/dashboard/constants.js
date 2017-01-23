3a08ce75587fd63ae1a9e3d0020bd8275e012ab0
Brian Weinreich <be.weinreich@gmail.com>
2017-01-23 15:38:50 -0500

    correct stripe key

--- src/scripts/dashboard/constants.js
+++ src/scripts/dashboard/constants.js
@@ -1,8 +1,8 @@
 export const ACCOUNTS_URL = "https://clerk.density.io";
 export const API_URL = "https://api.density.io/v1";
-export const BOOKIE_URL = "http://localhost:8000/v2";
+export const BOOKIE_URL = "https://bookie.density.io/v2";
 export const GA_TRACKING_CODE = "UA-77313135-1";
 export const INTEGRATIONS_URL = "https://density-integrations.herokuapp.com/v1";
 // export const INTEGRATIONS_URL = "http://localhost:3000/v1";
 export const SLACK_CLIENT_ID = "2180757426.123853628612";
-export const STRIPE_KEY = process.env.STRIPE_KEY || "pk_test_7I4XNXDTskv8K27CAbAxrCj5";
+export const STRIPE_KEY = "pk_live_00WyFCroFs6YFaY01TFLlNfj";
