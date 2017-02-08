ca2e97d92864f243a9dda24bd2b3fb3661f46acd
Brian Weinreich <be.weinreich@gmail.com>
2017-02-08 13:18:20 -0500

    biome

--- src/scripts/dashboard/constants.js
+++ src/scripts/dashboard/constants.js
@@ -5,5 +5,5 @@ export const API_URL = getEnvVariable(process.env.API_URL, 'string', true);
 export const BOOKIE_URL = getEnvVariable(process.env.BOOKIE_URL, 'string', true);
 export const GA_TRACKING_CODE = "UA-77313135-1";
 export const INTEGRATIONS_URL = getEnvVariable(process.env.INTEGRATIONS_URL, 'string', true);
-export const SLACK_CLIENT_ID = "2180757426.123853628612";
+export const SLACK_CLIENT_ID = getEnvVariable(process.env.SLACK_CLIENT_ID, 'string', true);
 export const STRIPE_KEY = getEnvVariable(process.env.STRIPE_KEY, 'string', true);
