d1e3efa1e268a0ee055aa511b715bf3ad6fbbdad
Brian Weinreich <be.weinreich@gmail.com>
2017-02-09 15:57:03 -0500

    exceeds

--- src/scripts/dashboard/components/AlertCard.js
+++ src/scripts/dashboard/components/AlertCard.js
@@ -54,7 +54,7 @@ function AlertCard({
               </select>
             </div>
           </div>
-          <div className="alert-line">equals 
+          <div className="alert-line">exceeds 
             <input type="number" defaultValue={alert.compare_value} onChange={onUpdateFormField('compareValue', alert.id)} disabled={enableFields} />
             people
           </div>
