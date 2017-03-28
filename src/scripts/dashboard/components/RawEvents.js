e139279b4f433a0e3c692b44ac34e5f8157ce06f
Brian Weinreich <be.weinreich@gmail.com>
2017-03-28 12:10:02 -0400

    get csv

--- src/scripts/dashboard/components/RawEvents.js
+++ src/scripts/dashboard/components/RawEvents.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Button} from '@blueprintjs/core';
-import {getCsv} from 'dashboard/actions/get-csv';
+import {getCsv} from 'dashboard/actions/csv';
 import moment from 'moment';
 
 import {rawEventsChangePage, rawEventsSetDateRange, rawEventsFetch} from 'dashboard/actions/raw-events';
