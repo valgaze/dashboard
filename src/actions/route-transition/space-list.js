2288f2cab25b8a48cda4450073dcfd45546f7b45
Ryan Gaus <rsg1egoman@gmail.com>
2017-06-27 08:45:53 -0400

    code: fetch doorways when space detail page loads

--- src/actions/route-transition/space-list.js
+++ src/actions/route-transition/space-list.js
@@ -1,5 +1,6 @@
 import { core } from '@density-int/client';
 import collectionSpacesSet from '../collection/spaces-set';
+import collectionDoorwaysSet from '../collection/doorways-set';
 
 export const ROUTE_TRANSITION_SPACE_LIST = 'ROUTE_TRANSITION_SPACE_LIST';
 
@@ -7,9 +8,14 @@ export default function routeTransitionSpaceList() {
   return dispatch => {
     dispatch({ type: ROUTE_TRANSITION_SPACE_LIST });
 
-    // Fetch a list of all spaces.
-    return core.spaces.list().then(spaces => {
+    return Promise.all([
+      // Fetch a list of all spaces.
+      core.spaces.list(),
+      // Fetch a list of all doorways.
+      core.doorways.list(),
+    ]).then(([spaces, doorways]) => {
       dispatch(collectionSpacesSet(spaces.results));
+      dispatch(collectionDoorwaysSet(doorways.results));
     });
   };
 }
