18f314c9ebb82c79757936b875011ebadc661448
Ryan Gaus <rsg1egoman@gmail.com>
2017-07-03 10:22:35 -0400

    code: add modal styles to the update doorway modal, too

--- src/components/environment-modal-update-doorway/index.js
+++ src/components/environment-modal-update-doorway/index.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Modal from '@density/ui-modal';
 import Card, { CardHeader, CardBody } from '@density/ui-card';
+import InputBox from '@density/ui-input-box';
+import ModalHeaderActionButton from '../modal-header-action-button/index';
 
 export default class EnvironmentModalUpdateDoorway extends React.Component {
   constructor(props) {
@@ -15,28 +17,34 @@ export default class EnvironmentModalUpdateDoorway extends React.Component {
     return <div className="environment-modal-update-doorway">
       <Modal onClickBackdrop={this.props.onDismiss}>
         <Card>
-          <CardHeader>Doorway Details</CardHeader>
+          <CardHeader>
+            Doorway Details
+            {this.state.isEditing ? <ModalHeaderActionButton
+              onClick={this.props.onDelete}
+            >Delete</ModalHeaderActionButton> : <ModalHeaderActionButton
+              onClick={() => this.setState({isEditing: true})}
+            >Edit</ModalHeaderActionButton>}
+          </CardHeader>
           <CardBody>
-            <p onClick={() => this.setState({isEditing: true})}>Edit</p>
-            <p onClick={this.props.onDelete}>Delete</p>
-
             <ul>
               <li>
                 <label htmlFor="update-doorway-name">Doorway Name</label>
-                <input
+                <InputBox
                   type="text"
                   id="update-doorway-name"
                   value={this.state.name}
                   onChange={e => this.setState({name: e.target.value})}
+                  disabled={!this.state.isEditing}
                 />
               </li>
               <li>
                 <label htmlFor="update-doorway-desc">Description</label>
-                <input
+                <InputBox
                   type="text"
                   id="update-doorway-description"
                   value={this.state.description}
                   onChange={e => this.setState({description: e.target.value})}
+                  disabled={!this.state.isEditing}
                 />
               </li>
             </ul>
