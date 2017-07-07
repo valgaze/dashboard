51a50c2badcd470eaff657e0e79e3ab1b445b7ee
Ryan Gaus <rsg1egoman@gmail.com>
2017-07-07 13:32:00 -0400

    code(test): tests for COLLECTION_TOKEN_PUSH

--- src/reducers/tokens/test.js
+++ src/reducers/tokens/test.js
@@ -2,6 +2,7 @@ import assert from 'assert';
 import tokens from './index';
 
 import collectionTokensSet from '../../actions/collection/tokens/set';
+import collectionTokensPush from '../../actions/collection/tokens/push';
 
 // Don't worry - these tokens are bogus.
 const TOKEN_ONE = 'tok_3wxsa6e8dh5zdnf73ubpnaq37wz2nawcjw8hh5sfawb';
@@ -23,6 +24,41 @@ describe('tokens', function() {
         {tokenType: 'readonly', key: TOKEN_ONE},
         {tokenType: 'readwrite', key: TOKEN_TWO},
       ],
+    });
+  });
+  it('should push token when given a token update', function() {
+    const initialState = tokens(undefined, {});
+
+    // Add a new token.
+    const tokenInCollection = tokens(initialState, collectionTokensPush({
+      id: 0,
+      name: 'foo',
+      token_type: 'readonly',
+    }));
+
+    // Update token in collection
+    const tokenUpdatedInCollection = tokens(tokenInCollection, collectionTokensPush({
+      id: 0,
+      name: 'new name',
+    }));
+
+    assert.deepEqual(tokenUpdatedInCollection, {
+      ...initialState,
+      data: [{id: 0, name: 'new name', tokenType: 'readonly'}],
+    });
+  });
+  it('should push token when given a new token', function() {
+    const initialState = tokens(undefined, {});
+
+    const result = tokens(initialState, collectionTokensPush({
+      id: 0,
+      name: 'foo',
+      tokenType: 'readonly',
+    }));
+
+    assert.deepEqual(result, {
+      ...initialState,
+      data: [{id: 0, name: 'foo', tokenType: 'readonly'}],
     });
   });
 });
