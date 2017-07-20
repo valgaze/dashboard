c84ff03ecfdf536f9ee08272d2e75024b068dfa7
Ryan Gaus <rsg1egoman@gmail.com>
2017-07-20 13:02:03 -0400

    code(design): make token cards match mockup

--- src/components/token-card/index.js
+++ src/components/token-card/index.js
@@ -1,18 +1,27 @@
 import * as React from 'react';
 import * as classnames from 'classnames';
 
+import Card, { CardHeader, CardBody } from '@density/ui-card';
+import ModalHeaderActionButton from '../modal-header-action-button/index';
+
 export default function TokenCard({
   token,
   tokenType,
 }) {
-  return <div className={classnames('token-card', {
-    'token-card-read-only': tokenType === 'readonly',
-    'token-card-read-write': tokenType === 'readwrite',
-  })}>
-    <h2>{token.name || '(untitled)'}</h2>
-    <div className="token-card-body">
+  return <Card>
+    <CardHeader>
+      {token.name}
+
+      <div className="token-card-permissions-badge">
+        {tokenType === 'readwrite' ? 'Read/Write' : 'Read Only'}
+      </div>
+
+      <ModalHeaderActionButton className="token-card-edit">Edit</ModalHeaderActionButton>
+    </CardHeader>
+    <CardBody>
       <p>{token.desc}</p>
-      <code>{token.key}</code>
-    </div>
-  </div>;
+      <code className="token-card-token-content">{token.key}</code>
+      <button className="token-card-copy-token-button">Copy Token</button>
+    </CardBody>
+  </Card>;
 }
