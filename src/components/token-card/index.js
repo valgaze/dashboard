0a5b6887f6017226a67354df57da8664011b6303
Ryan Gaus <rsg1egoman@gmail.com>
2017-07-21 12:54:38 -0400

    code: copy toke nto clipboard when the use clicks

--- src/components/token-card/index.js
+++ src/components/token-card/index.js
@@ -3,9 +3,7 @@ import * as React from 'react';
 import Card, { CardHeader, CardBody } from '@density/ui-card';
 import ModalHeaderActionButton from '../modal-header-action-button/index';
 
-export default function TokenCard({
-  token,
-}) {
+export default function TokenCard({token}) {
   return <Card>
     <CardHeader>
       {token.name}
@@ -19,7 +17,69 @@ export default function TokenCard({
     <CardBody>
       <p>{token.desc}</p>
       <code className="token-card-token-content">{token.key}</code>
-      <button className="token-card-copy-token-button">Copy Token</button>
+      <button
+        className="token-card-copy-token-button"
+        onClick={() => copyTextToClipboard(token.key)}
+      >Copy Token</button>
     </CardBody>
   </Card>;
 }
+
+
+
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  //
+  // *** This styling is an extra step which is likely not required. ***
+  //
+  // Why is it here? To ensure:
+  // 1. the element is able to have focus and selection.
+  // 2. if element was to flash render it has minimal visual impact.
+  // 3. less flakyness with selection and copying which **might** occur if
+  //    the textarea element is not visible.
+  //
+  // The likelihood is the element won't even render, not even a flash,
+  // so some of these are just precautions. However in IE the element
+  // is visible whilst the popup box asking the user for permission for
+  // the web page to copy to the clipboard.
+  //
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = 'transparent';
+
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+  } catch (err) {
+    console.error('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+}
+
