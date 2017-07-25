d473bf9fd91894a108c3bf950b85a5ef97b22ab6
Ryan Gaus <rsg1egoman@gmail.com>
2017-07-25 13:44:21 -0400

    code: redesign of login page to match internal admin login page

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -3,8 +3,9 @@ import classnames from 'classnames';
 import { connect } from 'react-redux';
 
 import { accounts } from '@density-int/client';
-
 import sessionTokenSet from '../../actions/session-token/set';
+
+import { InputStackItem, InputStackGroup } from '@density/ui-input-stack';
 
 export class Login extends React.Component {
   constructor(props) {
@@ -44,7 +45,6 @@ export class Login extends React.Component {
           src="https://dashboard.density.io/assets/images/density_mark_black.png"
           alt="Density Logo"
         />
-        <h1>Login</h1>
 
         {/* Render any errors with previous login attempts */}
         {this.state.error ? <span className="login-error">
@@ -52,29 +52,29 @@ export class Login extends React.Component {
         </span> : null}
 
         {/* Login inputs */}
-        <input
-          className="login-textbox"
-          type="email"
-          placeholder="Email Address"
-          onChange={e => this.setState({email: e.target.value})}
-          onKeyPress={this.onEnter.bind(this)}
-          value={this.state.email}
-        />
-        <input
-          className="login-textbox"
-          type="password"
-          placeholder="Password"
-          onChange={e => this.setState({password: e.target.value})}
-          onKeyPress={this.onEnter.bind(this)}
-          value={this.state.password}
-        />
+        <InputStackGroup className="login-form">
+          <InputStackItem
+            type="email"
+            placeholder="Email Address"
+            onChange={e => this.setState({email: e.target.value})}
+            onKeyPress={this.onEnter.bind(this)}
+            value={this.state.email}
+          />
+          <InputStackItem
+            type="password"
+            placeholder="Password"
+            onChange={e => this.setState({password: e.target.value})}
+            onKeyPress={this.onEnter.bind(this)}
+            value={this.state.password}
+          />
+        </InputStackGroup>
         <button 
           className={classnames('login-submit-button', {loading: this.state.loading})}
           onClick={this.onLogin.bind(this)}
           type="button"
           disabled={this.state.loading}
         >
-          <span className="label">Log in</span>
+          <span className="label">Login</span>
           {this.state.loading ? <img
             className="loading-image"
             src="/assets/images/loading.gif"
