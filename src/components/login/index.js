363ac97e55bd56bbf22f03dbffeb86cda7f0f377
Ryan Gaus <rsg1egoman@gmail.com>
2017-08-01 11:30:25 -0400

    code: forgot password flow - 1st step
    - User clicks "forgot password" on login page
    - Login page component displays a password reset form.
    - User types email into the component.
    - Component makes a request to clerk, which sends the forgot password
    email.
    - Display a success message.

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -7,10 +7,13 @@ import sessionTokenSet from '../../actions/session-token/set';
 
 import { InputStackItem, InputStackGroup } from '@density/ui-input-stack';
 
+const LOGIN = 'LOGIN', FORGOT_PASSWORD = 'FORGOT_PASSWORD';
+
 export class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      view: LOGIN,
       email: '',
       password: '',
       loading: false,
@@ -25,16 +28,112 @@ export class Login extends React.Component {
   }
 
   onLogin() {
-    this.setState({loading: true});
+    this.setState({loading: true, error: null});
     return accounts.users.login({
       email: this.state.email,
       password: this.state.password,
     }).then(token => {
-      this.setState({loading: false});
+      this.setState({loading: false, error: null});
       this.props.onUserSuccessfullyLoggedIn(token);
     }).catch(error => {
       this.setState({loading: false, error: error.message});
     });
+  }
+
+  onForgotPassword() {
+    this.setState({loading: true, error: null});
+    return accounts.users.password_forgot({
+      email: this.state.email,
+    }).then(resp => {
+      this.setState({loading: false, error: null, forgotPasswordConfirmation: resp.message});
+    }).catch(error => {
+      this.setState({loading: false, error: error.message});
+    });
+  }
+
+  renderLoginForm() {
+    return <div className="login-form-container">
+      {/* Input stack used to enter login info */}
+      <InputStackGroup className="login-form">
+        <InputStackItem
+          type="email"
+          placeholder="Email Address"
+          invalid={this.state.email.length > 0 && this.state.email.indexOf('@') === -1}
+          onChange={e => this.setState({email: e.target.value})}
+          onKeyPress={this.onEnter.bind(this)}
+          value={this.state.email}
+        />
+        <InputStackItem
+          type="password"
+          placeholder="Password"
+          onChange={e => this.setState({password: e.target.value})}
+          onKeyPress={this.onEnter.bind(this)}
+          value={this.state.password}
+        />
+      </InputStackGroup>
+
+      {/* Move to forgot password view */}
+      <div
+        className="login-forgot-password-link"
+        onClick={() => this.setState({view: FORGOT_PASSWORD, error: null})}
+      >Forgot Password</div>
+
+      {/* Submit the form! */}
+      <button 
+        className={classnames('login-submit-button', {loading: this.state.loading})}
+        onClick={this.onLogin.bind(this)}
+        type="button"
+        disabled={this.state.loading || this.state.email.indexOf('@') === -1}
+      >
+        <span className="label">Login</span>
+        {this.state.loading ? <img
+          className="loading-image"
+          src="/assets/images/loading.gif"
+          alt="Loading"
+        /> : null}
+      </button>
+    </div>;
+  }
+
+  renderForgotPasswordForm() {
+    return <div>
+      {/* Move to back to login page */}
+      <span onClick={() => this.setState({view: LOGIN, error: null})}>Back to login</span>
+
+      <p>
+        You forgot your password? Enter your email below and we'll send you the old password reset
+        email thingy:
+      </p>
+      <InputStackGroup className="login-password-reset-form">
+        <InputStackItem
+          type="email"
+          placeholder="Email Address"
+          invalid={this.state.email.length > 0 && this.state.email.indexOf('@') === -1}
+          onChange={e => this.setState({email: e.target.value, error: null})}
+          onKeyPress={this.onEnter.bind(this)}
+          value={this.state.email}
+        />
+      </InputStackGroup>
+
+      {this.state.forgotPasswordConfirmation ? <span className="login-forgot-password-message">
+        {this.state.forgotPasswordConfirmation}
+      </span> : null}
+
+      {/* Submit the form! */}
+      <button 
+        className={classnames('login-submit-button', {loading: this.state.loading})}
+        onClick={this.onForgotPassword.bind(this)}
+        type="button"
+        disabled={this.state.loading || this.state.email.indexOf('@') === -1}
+      >
+        <span className="label">Forgot password</span>
+        {this.state.loading ? <img
+          className="loading-image"
+          src="/assets/images/loading.gif"
+          alt="Loading"
+        /> : null}
+      </button>
+    </div>;
   }
 
   render() {
@@ -52,36 +151,9 @@ export class Login extends React.Component {
         </span> : null}
 
         {/* Login inputs */}
-        <InputStackGroup className="login-form">
-          <InputStackItem
-            type="email"
-            placeholder="Email Address"
-            invalid={this.state.email.length > 0 && this.state.email.indexOf('@') === -1}
-            onChange={e => this.setState({email: e.target.value})}
-            onKeyPress={this.onEnter.bind(this)}
-            value={this.state.email}
-          />
-          <InputStackItem
-            type="password"
-            placeholder="Password"
-            onChange={e => this.setState({password: e.target.value})}
-            onKeyPress={this.onEnter.bind(this)}
-            value={this.state.password}
-          />
-        </InputStackGroup>
-        <button 
-          className={classnames('login-submit-button', {loading: this.state.loading})}
-          onClick={this.onLogin.bind(this)}
-          type="button"
-          disabled={this.state.loading || this.state.email.indexOf('@') === -1}
-        >
-          <span className="label">Login</span>
-          {this.state.loading ? <img
-            className="loading-image"
-            src="/assets/images/loading.gif"
-            alt="Loading"
-          /> : null}
-        </button>
+        {this.state.view === LOGIN ?
+          this.renderLoginForm.apply(this) :
+          this.renderForgotPasswordForm.apply(this)}
       </div>
     </div>;
   }
