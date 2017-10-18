11a2a9093f233de994333af97d0a0c317817e3ec
Brian Weinreich <be.weinreich@gmail.com>
2017-10-18 14:02:02 -0400

    code: testing out new mixpanel dash

--- src/helpers/mixpanel-user-reducer-enhancer/index.js
+++ src/helpers/mixpanel-user-reducer-enhancer/index.js
@@ -10,12 +10,12 @@ export default function mixpanelUserReducerEnhancer(reducer) {
 
       // Update the user on mixpanel if the user info changed.
       window.mixpanel.identify(result.user.id);
-      window.mixpanel.people.set(result.user.id, {
+      window.mixpanel.people.set({
          $first_name: result.user.fullName,
          $last_name: '',
-         email: result.user.email,
+         $email: result.user.email,
          organization_id: result.user.organization.id,
-         organization_name: result.user.organization.name,
+         organization: result.user.organization.name,
          is_admin: result.user.is_admin,
       });
     }
