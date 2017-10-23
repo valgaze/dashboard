39ecdb4fe906f461c388694cf95393256243c403
Brian Weinreich <be.weinreich@gmail.com>
2017-10-23 16:48:38 -0400

    remove test file

--- src/components/pilot/test.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { mount } from 'enzyme';
-import assert from 'assert';
-
-import { Provider } from 'react-redux';
-import storeFactory from '../../store';
-
-// TODO
