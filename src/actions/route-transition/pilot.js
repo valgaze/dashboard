e08c30e8dafff9a301b108738921cc98ee807538
Brian Weinreich <be.weinreich@gmail.com>
2017-10-22 23:05:20 -0400

    dynamically pulling down data

--- src/actions/route-transition/pilot.js
+++ src/actions/route-transition/pilot.js
@@ -1,4 +1,6 @@
+import { core, accounts } from '@density-int/client';
 import pilotUpdate from '../pilot/update';
+import pilotSet from '../pilot/set';
 
 export const ROUTE_TRANSITION_PILOT = 'ROUTE_TRANSITION_PILOT';
 
@@ -7,9 +9,24 @@ export default function routeTransitionPilot() {
     dispatch({ type: ROUTE_TRANSITION_PILOT });
 
     let state = getState();
-    const doorways = state.pilot.doorways;
-    for (var i = 0; i < doorways.length; i++) {
-      dispatch(pilotUpdate(doorways[i]['id']));
-    }
+
+    accounts.users.me(state.sessionToken).then(data => data)
+    .then(data => {
+      let orgId = data['organization']['id'];
+      let configURL = "https://s3.amazonaws.com/dashboard.density.io/vids/"+orgId+".json";
+
+      fetch(configURL, {method: 'GET'})
+      .then(function(response) {
+        return response.json();
+      }).then(function(json) {
+        dispatch(pilotSet(json));
+        let doorways = json['doorways'];
+        for (var i = 0; i < doorways.length; i++) {
+          dispatch(pilotUpdate(doorways[i]['id']));
+        }
+      });
+
+    });
+    
   };
 }
--- src/reducers/pilot/index.js
+++ src/reducers/pilot/index.js
@@ -1,62 +1,12 @@
 import moment from 'moment';
 
+import { PILOT_SET } from '../../actions/pilot/set';
 import { PILOT_UPDATE } from '../../actions/pilot/update';
 
 const initialState = {
-  spaceId: "spc_275872166560399620",
+  spaceId: null,
   updated: new Date(),
-  doorways: [
-    {
-      id: "drw_100249459190923532",
-      name: "Elevator A Installation",
-      uptime: "99%",
-      accuracy: "97%",
-      humansPerHour: 0,
-      totalHumansSeen: 0,
-      rawVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/raw_elevator_a_compressed.mp4',
-      algoVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/algo_elevator_a_compressed.mp4'
-    },
-    {
-      id: "drw_9414389350269184",
-      name: "Elevator B Installation",
-      uptime: "99%",
-      accuracy: "95%",
-      humansPerHour: 0,
-      totalHumansSeen: 0,
-      rawVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/algo_elevator_a_compressed.mp4',
-      algoVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/algo_elevator_a_compressed.mp4'
-    },
-    {
-      id: "drw_102668554003808568",
-      name: "Glass Entry Installation",
-      uptime: "99%",
-      accuracy: "95%",
-      humansPerHour: 0,
-      totalHumansSeen: 0,
-      rawVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/algo_elevator_a_compressed.mp4',
-      algoVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/algo_elevator_a_compressed.mp4'
-    },
-    {
-      id: "drw_204050029856424200",
-      name: "Stairwell A Installation",
-      uptime: "99%",
-      accuracy: "95%",
-      humansPerHour: 0,
-      totalHumansSeen: 0,
-      rawVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/algo_elevator_a_compressed.mp4',
-      algoVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/algo_elevator_a_compressed.mp4'
-    },
-    {
-      id: "drw_107491478137209132",
-      name: "Stairwell B Installation",
-      uptime: "99%",
-      accuracy: "95%",
-      humansPerHour: 0,
-      totalHumansSeen: 0,
-      rawVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/algo_elevator_a_compressed.mp4',
-      algoVideo: 'https://s3.amazonaws.com/dashboard.density.io/vids/algo_elevator_a_compressed.mp4'
-    }
-  ]
+  doorways: []
 };
 
 function doorwaysWithUpdatedCountAndHourly(doorways, doorwayId, totalEvents, sinceDate) {
@@ -74,6 +24,8 @@ function doorwaysWithUpdatedCountAndHourly(doorways, doorwayId, totalEvents, sin
 
 export default function pilot(state=initialState, action) {
   switch (action.type) {
+  case PILOT_SET:
+    return action.data;
   case PILOT_UPDATE:
     return {...state, updated: new Date(), doorways: doorwaysWithUpdatedCountAndHourly(state.doorways, action.doorwayId, action.totalEvents, action.sinceDate)}
   default:
