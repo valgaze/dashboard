a39721fa0186fbe098771c270604d2fd4e8a27af
Ryan Gaus <rsg1egoman@gmail.com>
2017-11-15 17:07:17 -0500

    code(object-snake-to-camel): deeply nested keys

--- src/helpers/object-snake-to-camel/test.js
+++ src/helpers/object-snake-to-camel/test.js
@@ -15,5 +15,27 @@ describe('object-snake-to-camel', function() {
 
     assert.deepEqual(objectSnakeToCamel(input), actual);
   });
-});
+  it('should work for deeply nested objects', function() {
+    const actual = {
+      fooBar: 'baz',
+      alreadyCamel: 'bla',
+      deeplyNested: {
+        heyLook: 'value',
+        meToo: 'yea!',
+        butNot: 'me',
+      },
+    };
 
+    const input = {
+      foo_bar: 'baz',
+      alreadyCamel: 'bla',
+      deeply_nested: {
+        hey_look: 'value',
+        me_too: 'yea!',
+        butNot: 'me',
+      },
+    };
+
+    assert.deepEqual(objectSnakeToCamel(input), actual);
+  });
+});
