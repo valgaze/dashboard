c36e430f6e19454430198033b7fb42fd221dd6aa
Ryan Gaus <rsg1egoman@gmail.com>
2017-12-05 17:09:18 -0500

    chore: update api client to contain custom methods
    This is a workaround - I need to rethink how ajax requests are made.

--- src/client/index.js
+++ src/client/index.js
@@ -11,6 +11,42 @@ var core = clientele(Object.assign({}, require('./specs/core-api'), {errorFormat
 var accounts = clientele(Object.assign({}, require('./specs/accounts-api'), {errorFormatter: errorFormatter}));
 var metrics = clientele(Object.assign({}, require('./specs/metrics-api'), {errorFormatter: errorFormatter}));
 
+core.doorways.create = function(data) {
+  return window.fetch(`${core.config().core}/doorways?environment=True`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${core.config().token}`,
+    },
+    body: JSON.stringify(data),
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return errorFormatter(response);
+    }
+  });
+};
+
+core.doorways.update = function(data) {
+  return window.fetch(`${core.config().core}/doorways/${data.id}?environment=True`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${core.config().token}`,
+    },
+    body: JSON.stringify(data),
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return errorFormatter(response);
+    }
+  });
+};
+
 module.exports = {
   default: core,
   core: core,
