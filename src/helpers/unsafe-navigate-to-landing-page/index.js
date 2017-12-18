e49c44df71b79d6cf3e41aabf6077df62a46b19d
Gus Cost <guscost@gmail.com>
2017-12-18 14:50:15 -0500

    code: fix landing page redirect

--- src/helpers/unsafe-navigate-to-landing-page/index.js
+++ src/helpers/unsafe-navigate-to-landing-page/index.js
@@ -1,9 +1,14 @@
 import stringToBoolean from '../string-to-boolean/index';
 
 export default function unsafeNavigateToLandingPage(insightsPageLocked) {
-  // If the visualizations page is locked, instead redirect to the onboarding flow.
-  if (stringToBoolean(insightsPageLocked)) {
+  
+  // If there is a URL hash present, don't redirect to any default page
+  if (['', '#', '#/'].indexOf(window.location.hash) < 0) {
+    return;
+  // If the insights page is locked, redirect to the onboarding flow.
+  } else if (stringToBoolean(insightsPageLocked)) {
     window.location.hash = '#/account/setup/overview';
+  // Otherwise land on the insights page
   } else {
     window.location.hash = '#/insights/spaces';
   }
