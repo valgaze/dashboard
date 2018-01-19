c25f925641d5d0a6a9de588f1b7e6c73eec4edc2
Gus Cost <guscost@gmail.com>
2018-01-19 16:29:42 -0500

    code: fix generate function to return padded time values

--- src/helpers/generate-reset-time-choices/index.js
+++ src/helpers/generate-reset-time-choices/index.js
@@ -5,7 +5,7 @@ export default function generateResetTimeChoices() {
   return [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23].map(function (hour) {
     const time = moment().startOf('day').add(hour, 'hour');
     return {
-      value: time.format('H:mm'),
+      value: time.format('HH:mm'),
       display: time.format('h:mm a')
     };
   });
