e2e13be196ccac60043fd400c46d86fbbc521079
Ryan Gaus <rsg1egoman@gmail.com>
2018-01-24 10:08:55 -0500

    code: show a message when a space that was navigated to doesn't exist

--- src/actions/route-transition/visualization-space-detail.js
+++ src/actions/route-transition/visualization-space-detail.js
@@ -1,14 +1,18 @@
 import { core } from '../../client';
 import collectionSpacesPush from '../collection/spaces/push';
+import collectionSpacesError from '../collection/spaces/error';
 
 export const ROUTE_TRANSITION_VISUALIZATION_SPACE_DETAIL = 'ROUTE_TRANSITION_VISUALIZATION_SPACE_DETAIL';
 
 export default function routeTransitionSpaceDetail(id) {
-  return dispatch => {
+  return async dispatch => {
     dispatch({ type: ROUTE_TRANSITION_VISUALIZATION_SPACE_DETAIL, id })
 
-    return core.spaces.get({id}).then(space => {
+    try {
+      const space = await core.spaces.get({id})
       dispatch(collectionSpacesPush(space));
-    });
+    } catch (err) {
+      dispatch(collectionSpacesError(`Error loading space: ${err}`));
+    }
   }
 }
