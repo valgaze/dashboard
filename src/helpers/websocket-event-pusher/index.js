2e2574c61c701da03563c3c33977be3dd80b1511
Ryan Gaus <rsg1egoman@gmail.com>
2018-02-23 14:33:37 -0500

    code: add exponential backoff to event socket code

--- src/helpers/websocket-event-pusher/index.js
+++ src/helpers/websocket-event-pusher/index.js
@@ -4,22 +4,34 @@ import objectSnakeToCamel from '../object-snake-to-camel/index';
 import { core } from '../../client';
 
 // If disconnected, try to connect at minimum this often.
-const MINIMUM_CONNECTION_INTERVAL = 5000;
+const MINIMUM_CONNECTION_INTERVAL = 1000;
 
 // Conenct the the websocket.
 let ws;
 const events = new EventEmitter();
-export function connect(WebSocket) {
+export function connect(WebSocket, iteration=0) {
   if (WebSocket && host && token) {
     return core.sockets.create().then(({url}) => {
       ws = new WebSocket(url);
-      ws.onopen = () => events.emit('connected');
+      ws.onopen = () => {
+        // When a successful connection occurs, reset the iteration count back to zero.
+        iteration = 0;
+        events.emit('connected');
+      };
 
       // Currently, the only events are space updates.
       ws.onmessage = e => events.emit('space', objectSnakeToCamel(JSON.parse(e.data)));
 
       // When connection disconnects, reconnect after a delay.
-      ws.onclose = () => setTimeout(() => connect(WebSocket), MINIMUM_CONNECTION_INTERVAL);
+      ws.onclose = () => {
+        events.emit('disconnect');
+
+        // Calculate the timeout before the next reconnect attempt. Use an exponential backoff.
+        const backoffTimeout = MINIMUM_CONNECTION_INTERVAL + (Math.pow(iteration, 2) * 1000);
+
+        // Queue up the next attempt to reconnect to the socket server.
+        setTimeout(() => connect(WebSocket, iteration+1), backoffTimeout);
+      };
     });
   } else {
     return false;
