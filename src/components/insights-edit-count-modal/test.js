27249d2c4ff0fca5e90f7d0895ad452c79613a21
Ryan Gaus <rsg1egoman@gmail.com>
2018-03-28 15:11:18 -0400

    chore: update tests to use new name of component, `insights-edit-count-modal`

--- src/components/insights-edit-count-modal/test.js
+++ src/components/insights-edit-count-modal/test.js
@@ -20,7 +20,7 @@ describe('Space update modal', function() {
     />);
 
     // Should render two tokens
-    assert.equal(component.find('.visualization-space-detail-space-update-modal').length, 1);
+    assert.equal(component.find('.insights-edit-count-modal').length, 1);
   });
 
   it('should have proper ui interactions and create a reset', async function() {
@@ -34,69 +34,69 @@ describe('Space update modal', function() {
     />);
 
     // The current count is set initially to the value passed into the component.
-    let value = component.find('.visualization-space-detail-space-update-modal-count-picker-label input').prop('value');
+    let value = component.find('.insights-edit-count-modal-count-picker-label input').prop('value');
     assert.equal(value, '5');
 
     // Click the add button
-    component.find('.visualization-space-detail-space-update-modal-count-button.add').simulate('click');
+    component.find('.insights-edit-count-modal-count-button.add').simulate('click');
 
     // The current count is one larger.
-    value = component.find('.visualization-space-detail-space-update-modal-count-picker-label input').prop('value');
+    value = component.find('.insights-edit-count-modal-count-picker-label input').prop('value');
     assert.equal(value, '6');
 
     // Click the subtract button, twice
-    component.find('.visualization-space-detail-space-update-modal-count-button.subtract').simulate('click');
-    component.find('.visualization-space-detail-space-update-modal-count-button.subtract').simulate('click');
+    component.find('.insights-edit-count-modal-count-button.subtract').simulate('click');
+    component.find('.insights-edit-count-modal-count-button.subtract').simulate('click');
 
     // The current count is two smaller.
-    value = component.find('.visualization-space-detail-space-update-modal-count-picker-label input').prop('value');
+    value = component.find('.insights-edit-count-modal-count-picker-label input').prop('value');
     assert.equal(value, '4');
 
     // Click 'reset to zero' link
-    component.find('.visualization-space-detail-space-update-modal-reset-count').simulate('click');
+    component.find('.insights-edit-count-modal-reset-count').simulate('click');
 
     // Count is then internally set to zero
-    value = component.find('.visualization-space-detail-space-update-modal-count-picker-label input').prop('value');
+    value = component.find('.insights-edit-count-modal-count-picker-label input').prop('value');
     assert.equal(value, '0');
 
     // Type a count to reset to into the textbox
-    component.find('.visualization-space-detail-space-update-modal-count-picker-label input').simulate('change', {target: {value: '100'}});
-    component.find('.visualization-space-detail-space-update-modal-count-picker-label input').simulate('blur');
+    component.find('.insights-edit-count-modal-count-picker-label input').simulate('change', {target: {value: '100'}});
+    component.find('.insights-edit-count-modal-count-picker-label input').simulate('blur');
 
     // Verify the count was set to that number
-    value = component.find('.visualization-space-detail-space-update-modal-count-picker-label input').prop('value');
+    value = component.find('.insights-edit-count-modal-count-picker-label input').prop('value');
     assert.equal(value, '100');
 
     // Type a negative number into the textbox
-    component.find('.visualization-space-detail-space-update-modal-count-picker-label input').simulate('change', {target: {value: '-100'}});
-    component.find('.visualization-space-detail-space-update-modal-count-picker-label input').simulate('blur');
+    component.find('.insights-edit-count-modal-count-picker-label input').simulate('change', {target: {value: '-100'}});
+    component.find('.insights-edit-count-modal-count-picker-label input').simulate('blur');
 
     // Verify the count was set to zero
-    value = component.find('.visualization-space-detail-space-update-modal-count-picker-label input').prop('value');
+    value = component.find('.insights-edit-count-modal-count-picker-label input').prop('value');
     assert.equal(value, '0');
 
     // Type a floating point number into the box
-    component.find('.visualization-space-detail-space-update-modal-count-picker-label input').simulate('change', {target: {value: '10.2'}});
-    component.find('.visualization-space-detail-space-update-modal-count-picker-label input').simulate('blur');
+    component.find('.insights-edit-count-modal-count-picker-label input').simulate('change', {target: {value: '10.2'}});
+    component.find('.insights-edit-count-modal-count-picker-label input').simulate('blur');
 
     // Verify the count was set to ten
-    value = component.find('.visualization-space-detail-space-update-modal-count-picker-label input').prop('value');
+    value = component.find('.insights-edit-count-modal-count-picker-label input').prop('value');
     assert.equal(value, '10');
 
     // Type a text string into the box (this shouldn't happen because it's an input type number, but
     // it handles the case anyway)
-    component.find('.visualization-space-detail-space-update-modal-count-picker-label input').simulate('change', {target: {value: 'abc'}});
-    component.find('.visualization-space-detail-space-update-modal-count-picker-label input').simulate('blur');
+    component.find('.insights-edit-count-modal-count-picker-label input').simulate('change', {target: {value: 'abc'}});
+    component.find('.insights-edit-count-modal-count-picker-label input').simulate('blur');
 
     // Verify the count was unchanged
-    value = component.find('.visualization-space-detail-space-update-modal-count-picker-label input').prop('value');
+    value = component.find('.insights-edit-count-modal-count-picker-label input').prop('value');
     assert.equal(value, '10');
 
     // Set the count to a known state
     component.setState({count: 10, textCount: false});
 
     // Click the button in the modal, which should call `onSubmit` with the count
-    component.find('.visualization-space-detail-space-update-modal-submit').simulate('click');
+    component.find('.insights-edit-count-modal-submit').simulate('click');
 
     // Ensure that the correct count was passed through.
     assert.equal(onSubmit.firstCall.args[0], 10);
