f10864a128cb4f96c5eed5603ebe3b38000e26d8
Ryan Gaus <rsg1egoman@gmail.com>
2018-03-29 17:03:58 -0400

    chore: document raf fix

--- src/helpers/auto-refresh-hoc/index.js
+++ src/helpers/auto-refresh-hoc/index.js
@@ -25,33 +25,36 @@ export default function autoRefresh({interval, shouldComponentUpdate}) {
         this.state = { lastFrame: window.performance.now() };
         this.shouldComponentUpdate = shouldComponentUpdate || this.shouldComponentUpdate;
         this.tick = this.tick.bind(this);
-
-        // this.visiblilityChangeListener = () => {
-        // }
       }
       componentDidMount() {
         this.raf = window.requestAnimationFrame(this.tick);
-        // window.addEventListener('visibilitychange', this.visiblilityChangeListener);
       }
       componentWillUnmount() {
-        // window.removeEventListener('visibilitychange', this.visiblilityChangeListener);
         window.cancelAnimationFrame(this.raf);
       }
       tick() {
         const now = window.performance.now();
+
+        // Is the raf frame too old?
         if (this.state.lastFrame < now - AUTO_REFRESH_OLD_RAF_DELTA_IN_MILLISECONDS) {
+          // If so, set up a debounce interval to re-enable the raf.
           if (this.wakeUpRafAfterPageInactivity) {
             window.clearInterval(this.wakeUpRafAfterPageInactivity)
           }
           this.wakeUpRafAfterPageInactivity = window.setTimeout(() => {
+            // Restart raf loop
             this.setState({ lastFrame: performance.now() });
             this.raf = window.requestAnimationFrame(this.tick);
           }, AUTO_REFRESH_DEBOUNCE_TIMEOUT_IN_MILLISECONDS);
+
+          // And then skip this raf iteration.
           return
         }
 
+        // Re-render the component.
         this.setState({ lastFrame: now });
 
+        // And run the next frame.
         this.raf = window.requestAnimationFrame(this.tick);
       }
       render() {
