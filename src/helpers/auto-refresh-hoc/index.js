68ebef5a3a3d6e9977d3393bfd897e399b335d6d
Ryan Gaus <rsg1egoman@gmail.com>
2018-03-29 16:49:53 -0400

    code: fix dashboard inactive request animation frame bug
    When the dashboard live list view becomes inactive (ie, the tab is
    no longer in the forground) and re-focused, a bunch of rafs would back
    up and cause the page to take a long time to recover.
    
    Originally, I thought that this was related to rafs firing on an
    inactive tab. However, I discovered (and what I had expected before) that
    by design they don't, since you can't see the page it doesn't make sense
    for them to fire anyway.
    
    In actuality, the rafs were all being created once the tab was
    re-focused. This explained why all of our previous approaches weren't
    working - if the raf is being created when the page is active, trying to
    keep them from being created when the page is inactive doesn't work.
    
    So, what happens now is that when a raf is fired, it looks at the last
    time a raf was fired. If it was over three seconds ago (at least right
    now), it will clear out all rafs that were created after the page was
    re-activated. Then, afterwards, a callback will be fired in a setTimeout
    that will re-instate the raf.

--- src/helpers/auto-refresh-hoc/index.js
+++ src/helpers/auto-refresh-hoc/index.js
@@ -1,5 +1,16 @@
 import * as React from 'react';
 
+// The age of a request animation frame from the current performance.now() value for it to be
+// considered "old" and discarded. This is primarily used to detect frame backups when the user
+// forgrounds the dashboard from a background tab. In other words, this value represents the oldest
+// possible frame that will be rendered.
+const AUTO_REFRESH_OLD_RAF_DELTA_IN_MILLISECONDS = 3000;
+
+// The timeout after a tab has started to issue non "old" raf frames (see above threshold) that the
+// raf loop is kicked off again. In other words, this is the delay after a tab becomes active again
+// that the raf starts up.
+const AUTO_REFRESH_DEBOUNCE_TIMEOUT_IN_MILLISECONDS = 200;
+
 /*
  * let Component = ({myProp}) => <span>{myProp}</span>;
  * let NewComponent = autoRefresh({updateEveryMilliseconds: 500})(Component);
@@ -11,29 +22,36 @@ export default function autoRefresh({interval, shouldComponentUpdate}) {
     class RealtimeComponent extends React.Component {
       constructor(props) {
         super(props);
-        this.state = { lastFrame: Date.now() };
+        this.state = { lastFrame: window.performance.now() };
         this.shouldComponentUpdate = shouldComponentUpdate || this.shouldComponentUpdate;
         this.tick = this.tick.bind(this);
 
-        this.visiblilityChangeListener = () => {
-          if (document.hidden) {
-            window.cancelAnimationFrame(this.raf);
-          } else {
-            this.raf = window.requestAnimationFrame(this.tick);
-          }
-        }
+        // this.visiblilityChangeListener = () => {
+        // }
       }
       componentDidMount() {
         this.raf = window.requestAnimationFrame(this.tick);
-        window.addEventListener('visibilitychange', this.visiblilityChangeListener);
+        // window.addEventListener('visibilitychange', this.visiblilityChangeListener);
       }
       componentWillUnmount() {
-        window.removeEventListener('visibilitychange', this.visiblilityChangeListener);
+        // window.removeEventListener('visibilitychange', this.visiblilityChangeListener);
         window.cancelAnimationFrame(this.raf);
       }
       tick() {
-        var now = Date.now();
-        if (now - this.state.lastFrame > interval) { this.setState({ lastFrame: now }); }
+        const now = window.performance.now();
+        if (this.state.lastFrame < now - AUTO_REFRESH_OLD_RAF_DELTA_IN_MILLISECONDS) {
+          if (this.wakeUpRafAfterPageInactivity) {
+            window.clearInterval(this.wakeUpRafAfterPageInactivity)
+          }
+          this.wakeUpRafAfterPageInactivity = window.setTimeout(() => {
+            this.setState({ lastFrame: performance.now() });
+            this.raf = window.requestAnimationFrame(this.tick);
+          }, AUTO_REFRESH_DEBOUNCE_TIMEOUT_IN_MILLISECONDS);
+          return
+        }
+
+        this.setState({ lastFrame: now });
+
         this.raf = window.requestAnimationFrame(this.tick);
       }
       render() {
