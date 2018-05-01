0c9cfdd06a1c7ff09e4eeb32adb681d584a3d506
Ryan Gaus <rsg1egoman@gmail.com>
2018-05-01 11:01:38 -0400

    code(test): add test for user reducer loading boolean

--- src/reducers/user/test.js
+++ src/reducers/user/test.js
@@ -13,4 +13,9 @@ describe('user', function() {
     const output = user({user: {foo: 'bar'}}, userPush({email: 'test@density.io'}));
     assert.deepEqual(output.data, {foo: 'bar', email: 'test@density.io'});
   });
+
+  it('should fully update the user inside and reset loading state', function() {
+    const output = user({loading: true}, userSet({email: 'test@density.io'}));
+    assert.deepEqual(output.loading, false);
+  });
 });
