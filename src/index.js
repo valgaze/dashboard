134ab34b3bb76306530bb941f12fb7909eb045fd
Ryan Gaus <rsg1egoman@gmail.com>
2018-05-31 11:18:08 -0400

    Revert "code: remove service worker"
    This reverts commit 6865ab4346e9d023b75f2d1fd1421bb80f3910d3.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 import './built-css/styles.css';
 import { core, accounts } from './client';
 import ReactGA from 'react-ga';
@@ -290,3 +291,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+registerServiceWorker();
