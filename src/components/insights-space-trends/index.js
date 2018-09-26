00c11b13ab70156330d5d25696a7dc296cf79e7d
Ryan Gaus <rsg1egoman@gmail.com>
2018-09-26 16:23:44 -0400

    code(hotfix): adjust select box logic to render correct time range next to time segment group.
    Previously, the time segment's time wasn't being calculated for each
    time segment group - instead, the selected time segment group value was
    being used.
    
    Effectively, the calculation was being done once. It needed to be done
    `n` times, once per item in the list.

--- src/components/insights-space-trends/index.js
+++ src/components/insights-space-trends/index.js
@@ -85,24 +85,30 @@ function InsightsSpaceTrends({
             type="select"
             className="insights-space-trends-time-segment-box"
             value={selectedTimeSegmentGroup.id}
-            choices={timeSegmentGroupArray.map(ts => ({
-              id: ts.id,
-              label: `${ts.name} (${(
-                moment.utc()
-                  .tz(space.timeZone)
-                  .startOf('day')
-                  .add(parseTimeInTimeSegmentToSeconds(applicableTimeSegment.start), 'seconds')
-                  .format('h:mma')
-                  .slice(0, -1) /* am -> a */
-              )} - ${(
-                moment.utc()
-                  .tz(space.timeZone)
-                  .startOf('day')
-                  .add(parseTimeInTimeSegmentToSeconds(applicableTimeSegment.end), 'seconds')
-                  .format('h:mma')
-                  .slice(0, -1) /* am -> a */
-              )})`,
-            }))}
+            choices={timeSegmentGroupArray.map(ts => {
+              const applicableTimeSegmentForGroup = findTimeSegmentInTimeSegmentGroupForSpace(
+                ts,
+                space,
+              );
+              return {
+                id: ts.id,
+                label: `${ts.name} (${(
+                  moment.utc()
+                    .tz(space.timeZone)
+                    .startOf('day')
+                    .add(parseTimeInTimeSegmentToSeconds(applicableTimeSegmentForGroup.start), 'seconds')
+                    .format('h:mma')
+                    .slice(0, -1) /* am -> a */
+                )} - ${(
+                  moment.utc()
+                    .tz(space.timeZone)
+                    .startOf('day')
+                    .add(parseTimeInTimeSegmentToSeconds(applicableTimeSegmentForGroup.end), 'seconds')
+                    .format('h:mma')
+                    .slice(0, -1) /* am -> a */
+                )})`,
+              };
+            })}
             onChange={value => onChangeSpaceFilter('timeSegmentGroupId', value.id)}
           />
         </InsightsFilterBarItem>
