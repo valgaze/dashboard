a23207d691eaeaf66288f9a720bf6906c443a5c1
Ryan Gaus <rsg1egoman@gmail.com>
2018-10-09 09:25:27 -0400

    code: adjusted time segments to only every be at the start of end of a region

--- src/helpers/common-ranges/index.js
+++ src/helpers/common-ranges/index.js
@@ -16,61 +16,61 @@ export const WEEK_TO_DATE = 'WEEK_TO_DATE',
 // name, startDate, and endDate used by the DateRangePicker
 // key used to filter ranges in the dashboard
 export default function getCommonRangesForSpace(space) {
-  const NOW = getCurrentLocalTimeAtSpace(space);
+  const YESTERDAY = getCurrentLocalTimeAtSpace(space).subtract(1, 'day').endOf('day');
   return [
     {
       id: WEEK_TO_DATE,
       name: 'Week to date',
       startDate: getCurrentLocalTimeAtSpace(space).startOf('week'),
-      endDate: NOW,
+      endDate: YESTERDAY,
     },
     {
       id: MONTH_TO_DATE,
       name: 'Month to date',
       startDate: getCurrentLocalTimeAtSpace(space).startOf('month'),
-      endDate: NOW,
+      endDate: YESTERDAY,
     },
     {
       id: QUARTER_TO_DATE,
       name: 'Quarter to date',
       startDate: getCurrentLocalTimeAtSpace(space).startOf('quarter'),
-      endDate: NOW,
+      endDate: YESTERDAY,
     },
     {
       id: LAST_WEEK,
       name: 'Last week',
-      startDate: getCurrentLocalTimeAtSpace(space).startOf('week').subtract(1, 'week'),
-      endDate: NOW,
+      startDate: getCurrentLocalTimeAtSpace(space).subtract(1, 'week').startOf('week'),
+      endDate: getCurrentLocalTimeAtSpace(space).subtract(1, 'week').endOf('week'),
     },
     {
       id: LAST_MONTH,
       name: 'Last month',
-      startDate: getCurrentLocalTimeAtSpace(space).startOf('month').subtract(1, 'month'),
-      endDate: NOW,
+      startDate: getCurrentLocalTimeAtSpace(space).subtract(1, 'month').startOf('month'),
+      endDate: getCurrentLocalTimeAtSpace(space).subtract(1, 'month').endOf('month'),
     },
     {
       id: LAST_QUARTER,
       name: 'Last Quarter',
-      startDate: getCurrentLocalTimeAtSpace(space).startOf('quarter').subtract(1, 'quarter'),
-      endDate: NOW,
+      startDate: getCurrentLocalTimeAtSpace(space).subtract(1, 'quarter').startOf('quarter'),
+      endDate: getCurrentLocalTimeAtSpace(space).subtract(1, 'quarter').endOf('quarter'),
     },
     {
       id: LAST_7_DAYS,
       name: 'Last 7 days',
-      startDate: getCurrentLocalTimeAtSpace(space).subtract(1, 'week'),
-      endDate: NOW,
+      startDate: getCurrentLocalTimeAtSpace(space).subtract(7, 'days').startOf('day'),
+      endDate: YESTERDAY,
     },
     {
       id: LAST_30_DAYS,
       name: 'Last 30 days',
-      startDate: getCurrentLocalTimeAtSpace(space).subtract(1, 'month'),
-      endDate: NOW,
+      startDate: getCurrentLocalTimeAtSpace(space).subtract(30, 'days').startOf('day'),
+      endDate: YESTERDAY,
     },
     {
       id: LAST_90_DAYS,
       name: 'Last 90 days',
-      startDate: getCurrentLocalTimeAtSpace(space).subtract(1, 'quarter'),
-      endDate: NOW,
+      startDate: getCurrentLocalTimeAtSpace(space).subtract(90, 'days').startOf('day'),
+      endDate: YESTERDAY,
     },
   ];
 }
