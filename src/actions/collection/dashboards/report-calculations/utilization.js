886a8aa85493f23ceab60b7117943e1e8b36906b
Ryan Gaus <rsg1egoman@gmail.com>
2018-11-26 12:16:35 -0500

    chore: remove unused moment import

--- src/actions/collection/dashboards/report-calculations/utilization.js
+++ src/actions/collection/dashboards/report-calculations/utilization.js
@@ -1,4 +1,3 @@
-import moment from 'moment';
 import fetchAllPages from '../../../../helpers/fetch-all-pages/index';
 import objectSnakeToCamel from '../../../../helpers/object-snake-to-camel/index';
 import {
