4a0a08ae0f9773655a8d0400d6bd6f6944fc9a99
Ryan Gaus <rsg1egoman@gmail.com>
2018-11-27 11:18:54 -0500

    code: add ie 11 polyfill

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'react-app-polyfill/ie11'; // For IE 11 support
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { unregister as unregisterServiceWorker } from './registerServiceWorker';
