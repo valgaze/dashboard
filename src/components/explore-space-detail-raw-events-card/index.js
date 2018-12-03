46d387b3911fdf0135e5ef515b97871ce78129a2
Gus Cost <guscost@gmail.com>
2018-12-03 16:24:46 -0500

    hotfix: also safely map null raw events data

--- src/components/explore-space-detail-raw-events-card/index.js
+++ src/components/explore-space-detail-raw-events-card/index.js
@@ -57,7 +57,7 @@ export function ExploreSpaceDetailRawEventsCard({
 
         {calculatedData.state === 'COMPLETE' ? <CardTable
           headings={["Timestamp", "Event", "Doorway"]}
-          data={calculatedData.data.data}
+          data={calculatedData.data.data || []}
           mapDataItemToRow={item => [
             parseISOTimeAtSpace(item.timestamp, space).format('MMM Do YYYY, h:mm:ss a'),
             item.direction === 1 ? 'Entrance' : 'Exit',
