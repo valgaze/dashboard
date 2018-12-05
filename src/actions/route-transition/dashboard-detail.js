ffab071bdcb4649b21f94f617365be818a5c0ded
Ryan Gaus <rsg1egoman@gmail.com>
2018-12-05 07:45:47 -0500

    code: optimistically load dashboards during the route transition
    This ensures that there isn't a loading period in between switching
    dashboards.

--- src/actions/route-transition/dashboard-detail.js
+++ src/actions/route-transition/dashboard-detail.js
@@ -9,11 +9,25 @@ import fetchAllPages from '../../helpers/fetch-all-pages/index';
 export const ROUTE_TRANSITION_DASHBOARD_DETAIL = 'ROUTE_TRANSITION_DASHBOARD_DETAIL';
 
 export default function routeTransitionDashboardDetail(id) {
-  return async dispatch => {
+  return async (dispatch, getState) => {
     dispatch({ type: ROUTE_TRANSITION_DASHBOARD_DETAIL });
+
+    let dashboardSelectionPromise;
+
+    // First, if the dashboard already is in the collection, then immediately select it.
+    let selectedDashboard = getState().dashboards.data.find(d => d.id === id);
+    if (selectedDashboard) {
+      // The data for the dashboard being selected already exists, so don't "reset the state" as
+      // this will show a loading state / remove all dashboards that are already in the collection
+      // and that's not desired.
+      dashboardSelectionPromise = dispatch(collectionDashboardsSelect(selectedDashboard));
+    }
 
     // Though this route is only to load a single dashboard, we need to load all dashboards in order
     // to render the list on the left side of the screen.
+    //
+    // So, next, load all dashboards. Even if all dashboards are set, loading them again isn't a bad
+    // idea and ensures we have up to date data.
 
     let dashboards;
     try {
@@ -31,12 +45,18 @@ export default function routeTransitionDashboardDetail(id) {
       return;
     }
 
-    // If the dashboard that should be selected is in the response, then select it
-    const selectedDashboard = results.find(dashboard => dashboard.id === id);
+    // Now, if the dashboard to be selected hasn't already been selected (optimistically, at the top
+    // of the function), then select it now that it has been loaded.
     if (!selectedDashboard) {
-      console.warn('Do the equivelent of a 404 not found type of error here');
-      return;
+      selectedDashboard = results.find(dashboard => dashboard.id === id);
+      if (!selectedDashboard) {
+        console.warn('Do the equivelent of a 404 not found type of error here');
+        return;
+      }
+      dashboardSelectionPromise = dispatch(collectionDashboardsSelect(selectedDashboard));
     }
-    await dispatch(collectionDashboardsSelect(selectedDashboard));
+
+    // Wait for the dashboard selection to complete
+    await dashboardSelectionPromise;
   };
 }
