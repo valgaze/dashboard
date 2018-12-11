7d36c2eb745fa317fd9c8b885add8b8ba80cd5f6
Gus Cost <guscost@gmail.com>
2018-12-10 22:28:40 -0500

    code: rename .app-nav to .app-navbar, consistent breakpoints

--- src/components/app-nav/README.md
+++
@@ -1,7 +0,0 @@
-# AppBar
-
-A component for rendering an "app bar" at the top of the screen. This is a placeholder at the
-moment.
-
-## Component Props
-(documentation coming soon!)
--- src/components/app-navbar/index.js
+++ src/components/app-navbar/index.js
@@ -9,13 +9,13 @@ import {
   IconCopy
 } from '@density/ui-icons';
 
-function AppNavItem({isSelected, path, icon, name}) {
+function AppNavbarItem({isSelected, path, icon, name}) {
   const selected = isSelected();
   const Icon = icon;
   return (
-    <li className={classnames('app-nav-item', { selected })}>
+    <li className={classnames('app-navbar-item', { selected })}>
       <a href={path}>
-        {icon ? <span className="app-nav-icon">
+        {icon ? <span className="app-navbar-icon">
           {selected ? <Icon color={colorVariables.brandPrimary} /> : <Icon />}
         </span> : null}
         {name}
@@ -24,44 +24,44 @@ function AppNavItem({isSelected, path, icon, name}) {
   );
 }
 
-export default function AppNav({page, onLogout, loggedIn}) {
+export default function AppNavbar({page, onLogout, loggedIn}) {
   return (
-    <div className="app-nav-container">
-      <div className="app-nav">
-        <ul className="app-nav-left">
-          <AppNavItem
+    <div className="app-navbar-container">
+      <div className="app-navbar">
+        <ul className="app-navbar-left">
+          <AppNavbarItem
             isSelected={() => ['DASHBOARD_LIST', 'DASHBOARD_DETAIL'].includes(page)}
             path="#/dashboards"
             icon={IconDashboards}
             name="Dashboards"
           />
-          <AppNavItem
+          <AppNavbarItem
             isSelected={() => ['EXPLORE_SPACE_LIST', 'EXPLORE_SPACE_DETAIL'].includes(page)}
             path="#/spaces/explore"
             icon={IconGlobe}
             name="Explore"
           />
-          <AppNavItem
+          <AppNavbarItem
             isSelected={() => ['LIVE_SPACE_LIST', 'LIVE_SPACE_DETAIL'].includes(page)}
             path="#/spaces/live"
             icon={IconStopWatch}
             name="Live"
           />
-          <AppNavItem
+          <AppNavbarItem
             isSelected={() => ['DEV_TOKEN_LIST', 'DEV_WEBHOOK_LIST'].includes(page)}
             path="#/dev/tokens"
             icon={IconCopy}
             name="Developer"
           />
-          <AppNavItem
+          <AppNavbarItem
             isSelected={() => ['ACCOUNT_SETUP_OVERVIEW', 'ACCOUNT_SETUP_DOORWAY_LIST', 'ACCOUNT_SETUP_DOORWAY_DETAIL'].includes(page)}
             path="#/onboarding"
             icon={IconCopy}
             name="Onboarding"
           />
         </ul>
-        <ul className="app-nav-right">
-          <AppNavItem
+        <ul className="app-navbar-right">
+          <AppNavbarItem
             isSelected={() => false}
             path="#/logout"
             name="Logout"
