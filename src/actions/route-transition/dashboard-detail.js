b68e21b74d6be2d7040d8aa74ca57e3fa231adcc
Gus Cost <guscost@gmail.com>
2018-12-12 11:54:09 -0500

    code: handle loading single "invisible" dashboard not present in list

--- src/actions/route-transition/dashboard-detail.js
+++ src/actions/route-transition/dashboard-detail.js
@@ -21,39 +21,47 @@ export default function routeTransitionDashboardDetail(id) {
       // this will show a loading state / remove all dashboards that are already in the collection
       // and that's not desired.
       dashboardSelectionPromise = dispatch(collectionDashboardsSelect(selectedDashboard));
-    }
-
-    // Though this route is only to load a single dashboard, we need to load all dashboards in order
-    // to render the list on the left side of the screen.
-    //
-    // So, next, load all dashboards. Even if all dashboards are set, loading them again isn't a bad
-    // idea and ensures we have up to date data.
-
-    let dashboards;
-    try {
-      dashboards = await fetchAllPages(page => core.dashboards.list({page, page_size: 5000}));
-    } catch (err) {
-      dispatch(collectionDashboardsError(err));
-      return;
-    }
-
-    const results = dashboards.map(objectSnakeToCamel);
-    dispatch(collectionDashboardsSet(results));
-
-    if (results.length === 0) {
-      console.warn('Display an error relating to when there are no dashboards and something about contacting your density account manager');
-      return;
-    }
-
-    // Now, if the dashboard to be selected hasn't already been selected (optimistically, at the top
-    // of the function), then select it now that it has been loaded.
-    if (!selectedDashboard) {
-      selectedDashboard = results.find(dashboard => dashboard.id === id);
-      if (!selectedDashboard) {
-        console.warn('Do the equivelent of a 404 not found type of error here');
+    } else {
+      // Though this route is only to load a single dashboard, we need to load all dashboards in order
+      // to render the list on the left side of the screen.
+      //
+      // So, next, load all dashboards. Even if all dashboards are set, loading them again isn't a bad
+      // idea and ensures we have up to date data.
+      let dashboards;
+      try {
+        dashboards = await fetchAllPages(page => core.dashboards.list({page, page_size: 5000}));
+      } catch (err) {
+        dispatch(collectionDashboardsError(err));
         return;
       }
-      dashboardSelectionPromise = dispatch(collectionDashboardsSelect(selectedDashboard));
+
+      if (dashboards.length === 0) {
+        console.warn('Display an error relating to when there are no dashboards and something about contacting your density account manager');
+        return;
+      } else {
+        dashboards = dashboards.map(objectSnakeToCamel);
+      }
+
+      // Load the selected dashboard directly, in case it is hidden
+      selectedDashboard = dashboards.find(d => d.id === id);
+      if (selectedDashboard) {
+        dispatch(collectionDashboardsSet(dashboards));
+        dashboardSelectionPromise = dispatch(collectionDashboardsSelect(selectedDashboard));
+      } else {
+        try {
+          selectedDashboard = objectSnakeToCamel(await core.dashboards.detail({id}));
+        } catch (err) {
+          dispatch(collectionDashboardsError(err));
+          return;
+        }
+        if (!selectedDashboard) {
+          console.warn('Do the equivelent of a 404 not found type of error here');
+          return;
+        }
+        dashboards.push(selectedDashboard);
+        dispatch(collectionDashboardsSet(dashboards));
+        dashboardSelectionPromise = dispatch(collectionDashboardsSelect(selectedDashboard));
+      }
     }
 
     // Wait for the dashboard selection to complete
