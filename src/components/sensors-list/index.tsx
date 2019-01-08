dff4f92e6e90c3caab420d11b7d1579eb1b2f9b4
beweinreich <be.weinreich@gmail.com>
2019-01-08 15:13:30 -0500

    sensors page

--- src/components/sensors-list/index.tsx
+++ src/components/sensors-list/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
+
 
 export function SensorsList({
   sensors,
+  spaces
 }) {
   let sortedSensors = sensors.data.sort(function(a, b){
    if (a.status < b.status)
@@ -15,17 +18,41 @@ export function SensorsList({
     {/* Show errors in the spaces collection. */}
 
     <div className="sensors-list-container">
-      <div className="sensors-list-row">
-        {sortedSensors.map(sensor => {
-          return <div className="sensors-list-item" key={sensor.serial_number}>
-            <div>{sensor.serialNumber}</div>
-            <div>{sensor.doorwayName}</div>
-            <div>{sensor.doorwayId}</div>
-            <div className={`sensor-status-${sensor.status}`}>{sensor.status}</div>
-            <div>{sensor.lastHeartbeat}</div>
-          </div>;
-        })}
+      <div className="sensors-list-header">
+        <h2>Sensor Status</h2>
       </div>
+      <table>
+        <thead>
+          <tr>
+            <td>Serial Number</td>
+            <td>Status</td>
+            <td>Doorway</td>
+            <td>Space(s)</td>
+            <td>Last Heartbeat</td>
+          </tr>
+        </thead>
+        <tbody>
+        {sortedSensors.map(sensor => {
+          
+          const filteredSpaces = spaces.data.filter(space => {
+            return space.doorways.map(doorway => {
+              return doorway.id
+            }).includes(sensor.doorwayId)
+          })
+
+          return <tr key={sensor.serial_number}>
+            <td>{sensor.serialNumber}</td>
+            <td className={`sensor-status-${sensor.status}`}>{sensor.status}</td>
+            <td>{sensor.doorwayName}</td>
+            <td>{ filteredSpaces.map(space => {
+              return <span className="spaceItem">{space.name}</span>
+            }) }
+            </td>
+            <td>{moment(sensor.lastHeartbeat).format("MMM D, h:mma")}</td>
+          </tr>;
+        })}
+        </tbody>
+      </table>
     </div>
   </div>;
 }
@@ -33,6 +60,7 @@ export function SensorsList({
 export default connect((state: any) => {
   return {
     sensors: state.sensors,
+    spaces: state.spaces
   };
 }, dispatch => {
   return {
